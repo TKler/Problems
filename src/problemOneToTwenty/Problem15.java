289ecffdfa9e722580adaa83c0dc2ae94eaa2649
felix <felix@felix-UL30VT>
2018-10-25 17:51:08 +0100

    added 11, 15, 18 without desired complexity

---
+++ src/problemOneToTwenty/Problem15.java
@@ -0,0 +1,28 @@
+package problemOneToTwenty;
+
+import java.util.Random;
+
+//Good morning! Here's your coding interview problem for today.
+//
+//This problem was asked by Facebook.
+//
+//Given a stream of elements too large to store in memory, pick a random 
+//element from the stream with uniform probability.
+public class Problem15
+{
+	int _n = 0;
+	Object _element;
+	Random _random= new Random();
+	
+//	@assert we get each next element from some driver
+	public void pickFromStream(Object nextElement)
+	{
+		_n++;
+		if(_random.nextInt(_n)+1 == _n)
+			_element = nextElement;
+	}
+}
+// This chooses the first element since random(1) -> 0
+// For all other elements, they get a chance of 1/n to get chosen, where n is the current number of seen elements.
+// This mean the first element is chosen at 1 but then has a 0.5 chance of being replaced followed by 0.33 etc. 
+//This should sum up that each element has a 1/n chance of being chosen where n is this time the total number of elements 
---
+++ src/problemOneToTwenty/Problem18FirstTry.java
@@ -0,0 +1,80 @@
+package problemOneToTwenty;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+//Good morning! Here's your coding interview problem for today.
+//
+//This problem was asked by Google.
+//
+//Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum values of each subarray of length k.
+//
+//For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we should get: [10, 7, 8, 8], since:
+//
+//    10 = max(10, 5, 2)
+//    7 = max(5, 2, 7)
+//    8 = max(2, 7, 8)
+//    8 = max(7, 8, 7)
+//
+//Do this in O(n) time and O(k) space. You can modify the input array in-place and you do not need to store the results. You can simply print them out as you compute them.
+public class Problem18FirstTry
+{
+//	@assert array != empty or null
+//	@assert subarraySize != 0
+	public int[] getMaximumValuesOfSubarrays(int[] array, int subarraySize)
+	{
+		ArrayList<Integer> result = new ArrayList<Integer>(array.length - subarraySize+1);
+
+		int windowMaxIndex = computeMaxIndex(array, subarraySize, 0);
+		
+		result.add(array[windowMaxIndex]);
+		
+		for(int i = 1; i < array.length - subarraySize + 1; i++)
+		{
+			int newIndex = i+subarraySize-1;
+			int newValue = array[newIndex];
+			int oldValue = array[windowMaxIndex];
+			
+			if(newValue >= oldValue)
+				windowMaxIndex = newIndex;
+			
+			if(windowMaxIndex == i-1)
+				windowMaxIndex = computeMaxIndex(array, subarraySize, i);
+			
+			result.add(array[windowMaxIndex]);
+		}
+		return result.stream().mapToInt(Integer::valueOf).toArray(); 
+	}
+
+//	@assert no out of bound
+	private int computeMaxIndex(int[] array, int subarraySize, int offSet)
+	{
+		int windowMaxIndex = offSet;
+		for(int i = offSet; i < offSet + subarraySize; i++)
+		{
+			if(array[i] >= array[windowMaxIndex])
+				windowMaxIndex = i;
+		}
+		return windowMaxIndex;
+	}
+	// This runs in O(c) space aside from the result structure
+	// As time goes, this runs in k steps for the initialization and then every time a new max
+	//  has to be calculated in k steps again which ... yeah can happen n-k times if we get a 
+	//  inverted array. (And we got n-k loop steps) wonderful so more like k + n-k * n-k. 
+	//  thats not in O(n).
+	
+	@Nested
+	public class TestProblem18FirstTry
+	{
+		@Test
+		void test()
+		{
+			assertArrayEquals(new int[] {10,7,8,8}, getMaximumValuesOfSubarrays
+					(new int[]{10, 5, 2, 7, 8, 7}, 3));
+		}
+	}
+}
