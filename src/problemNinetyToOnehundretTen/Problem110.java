177fc4806b0167574d2865ad5c0f06e7152072f4
felix <felix@felix-UL30VT>
2018-10-27 19:36:10 +0100

    Problem 92 and 110

---
+++ src/problemNinetyToOnehundretTen/Problem110.java
@@ -0,0 +1,54 @@
+package problemNinetyToOnehundretTen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Good morning! Here's your coding interview problem for today.
+//
+//This problem was asked by Facebook.
+//
+//Given a binary tree, return all paths from the root to leaves.
+//
+//For example, given the tree
+//
+//   1
+//  / \
+// 2   3
+//    / \
+//   4   5
+//
+//it should return [[1, 2], [1, 3, 4], [1, 3, 5]].
+public class Problem110
+{
+	List<List<Integer>> _result = new ArrayList<List<Integer>>();
+	
+	public List<List<Integer>> allPathsToLeaves(Node root)
+	{
+		step(root, new ArrayList<Integer>());
+		return _result;
+	}
+	
+	public void step(Node node, ArrayList<Integer> pathToHere)
+	{
+		if(node == null)
+			_result.add(pathToHere);
+		else
+		{
+			pathToHere.add(node.getValue());
+			step(node.getLeft(), pathToHere);
+			step(node.getRight(), pathToHere);
+		}
+	}
+	
+	public class Node
+	{
+		Node _left = null, _right = null;
+		int _value;
+		
+		//constructor and stuff
+		
+		public Node getLeft() {return _left;}
+		public Node getRight() {return _right;}
+		public int getValue() {return _value;}
+	}
+}
---
+++ src/problemNinetyToOnehundretTen/Problem92.java
@@ -0,0 +1,84 @@
+package problemNinetyToOnehundretTen;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+//Good morning! Here's your coding interview problem for today.
+//
+//This problem was asked by Airbnb.
+//
+//We're given a hashmap with a key courseId and value a list of courseIds, 
+//which represents that the prerequsite of courseId is courseIds. Return a sorted ordering 
+//of courses such that we can finish all courses.
+//
+//Return null if there is no such ordering.
+//
+//For example, given {'CSC300': ['CSC100', 'CSC200'], 'CSC200': ['CSC100'], 'CSC100': []}, 
+//should return ['CSC100', 'CSC200', 'CSCS300'].
+public class Problem92
+{
+	/*
+	 * Take the course we can do - the one without prerequisites - and delete the prerequisite from all other courses.
+	 * this is the simple stupid approach, we could also check the result list or something similar, but this works just fine
+	 */
+//	@assert map neither empty nor null;
+	public List<Integer> getCourseOrder(HashMap<Integer, List<Integer> > map)
+	{
+		List<Integer> result = new ArrayList<Integer>();
+		int len = map.size();
+		
+		for(int i = 0; i < len; i++)
+		{
+			Iterator<Entry<Integer, List<Integer>>> iter = map.entrySet().iterator();
+			while(iter.hasNext())
+			{
+				Entry<Integer, List<Integer>> entry = iter.next();
+				if(entry.getValue().size() == 0)
+				{
+					result.add(entry.getKey());
+					map.remove(entry.getKey());
+					removeCourseAsConstraints(map, entry.getKey());
+					break;
+				}
+			}
+		}
+		
+		if(map.size() == 0) 
+			return result;
+		else
+			return null;
+	}
+
+	private void removeCourseAsConstraints(HashMap<Integer, List<Integer>> map, Integer completedCourse)
+	{
+		for(List<Integer> list : map.values())
+			list.remove(completedCourse);
+	}
+	
+	@Nested
+	public class TestProblem92
+	{
+		@Test 
+		void test()
+		{
+			HashMap<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+			ArrayList<Integer> list1 = new ArrayList<Integer>();
+			list1.addAll(Arrays.asList(1, 2));
+			map.put(3, list1);
+			ArrayList<Integer> list2 = new ArrayList<Integer>();
+			list2.addAll(Arrays.asList(1));
+			map.put(2, list2);
+			map.put(1, new ArrayList<Integer>());
+			assertTrue(Arrays.asList(1,2,3).equals(getCourseOrder(map)));
+		}
+	}
+}
