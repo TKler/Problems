b51223110a17fefa2299e0a60fdf8d537c17bda5
felix <felix@jamelin.de>
2018-11-01 18:41:39 +0000

    nice InPlace Solution for 113 added. O(3)? additional space

--- src/p90to120/Problem113.java
+++ src/p90to120/Problem113.java
@@ -1,6 +1,9 @@
 package p90to120;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -14,40 +17,86 @@ import org.junit.jupiter.api.Test;
 //Follow-up: given a mutable string representation, can you perform this operation in-place?
 public class Problem113
 {
-	public String reverseWordOrder(String words)
+//	As to 114, no but that can be changed. you could also use some custom method or lambda instead of isWhitespace for the delimiter detection.
+
+	
+//	public String reverseWordOrder(String words)
+//	{
+//		String result = "";
+//		words = " " + words;
+//		
+//		int index = words.length()-1;
+//		while(index >= 0)
+//		{
+//			int wordEnd = index;
+//			
+//			while(!Character.isWhitespace(words.charAt(index)))
+//				index--;
+//			
+//			result = result + (words.substring(index, wordEnd+1));
+//			
+//			index--;
+//		}
+//		return result.substring(1, result.length());
+//	}
+//	
+	public char[] reverseWordOrderInPlace(char[] words)
 	{
-		String result = "";
-		words = " " + words;
+		words = inverseArea(words, 0, words.length-1);
 		
-		int index = words.length()-1;
-		while(index >= 0)
+		int wordStart = 0;
+		while(wordStart < words.length)
 		{
-			int wordEnd = index;
-			
-			while(!Character.isWhitespace(words.charAt(index)))
-				index--;
-			
-			result = result + (words.substring(index, wordEnd+1));
-			
-			index--;
+			int wordEnd = findDelimiter(words, wordStart);
+			words = inverseArea(words, wordStart, wordEnd-1);
+			wordStart = wordEnd + 1;
 		}
-		return result.substring(1, result.length());
+		return words;
 	}
 	
-//	Can be done in place, but sure is much more work if you really want to do it with only indicies and space efficient. 
-//	Otherwise you can identify and "safe" the last word and cut it to the front of the word, hopefully you got something like a List data structure and remove it at the end.
-	
-//	As to 114, no but that can be changed. you could also use some custom method or lambda instead of isWhitespace for the delimiter detection.
-	
+	private int findDelimiter(char[] words, int index)
+	{
+		while(index < words.length && !Character.isWhitespace(words[index]))
+			index++;
+		return index;
+	}
+
+	private char[] inverseArea(char[] words, int left, int right)
+	{
+		while(left < right)
+		{
+			words = swapChars(words, left, right);
+			left++;
+			right--;
+		}
+		return words;
+	}
+
+	private char[] swapChars(char[] words, int left, int right)
+	{
+		char b = words[left];
+		words[left] = words[right];
+		words[right] = b;
+		return words;
+	}
+
 	@Nested
 	class TestProblem113
 	{
+//		@Test
+//		void test()
+//		{
+//			String input = "hello world here";
+//			String output = "here world hello";
+//			assertEquals(output, reverseWordOrder(input));
+//		}
+		
 		@Test
-		void test()
+		void testInOrder()
 		{
-			String input = "hello world here";
-			String output = "here world hello";
-			assertEquals(output, reverseWordOrder(input));
+			char [] input ="hello world here".toCharArray();
+			char[] output = "here world hello".toCharArray();
+			assertTrue(Arrays.equals(output, reverseWordOrderInPlace(input)));
 		}
 	}
 }
