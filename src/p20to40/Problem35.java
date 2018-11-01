1ed130978278ae82abe0c25dc3d836581f32c236
felix <felix@jamelin.de>
2018-11-01 12:25:24 +0000

    added 35, 37 done. deleted empty 24

---
+++ src/p20to40/Problem35.java
@@ -0,0 +1,73 @@
+package p20to40;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+//Good morning! Here's your coding interview problem for today.
+//
+//This problem was asked by Google.
+//
+//Given an array of strictly the characters 'R', 'G', and 'B', segregate the values of the 
+//array so that all the Rs come first, the Gs come second, and the Bs come last. 
+//You can only swap elements of the array.
+//
+//Do this in linear time and in-place.
+//
+//For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should become 
+//['R', 'R', 'R', 'G', 'G', 'B', 'B'].
+public class Problem35
+{
+	public char[] sortArray(char[] array, char[] order)
+	{
+		int index = 0;
+		for(int i = 0; i < order.length-1; i++)
+		{
+			index = sortToLeft(array, order[i], index);
+		}
+		
+		return array;
+	}
+	
+	private int sortToLeft(char[] array, char c, int startIndex)
+	{
+		int left = startIndex; 
+		int right = array.length-1;
+		boolean flag = true;
+		
+		while(flag)
+		{
+			while(array[left] == c)
+				left++;
+			while(array[right] != c)
+				right--;
+			
+			if(left < right)
+				swapValuesAtIndizies(array, left, right);
+			else
+				flag = false;
+		}
+		return right+1;
+	}
+
+	private void swapValuesAtIndizies(char[] array, int left, int right)
+	{
+		char safe = array[right];
+		array[right] = array[left];
+		array[left] = safe;
+	}
+	
+	@Nested
+	public class TestProblem35
+	{
+		@Test
+		void test()
+		{
+			char[] input = new char[] {'G', 'B', 'R', 'R', 'B', 'R', 'G'};
+			char[] output = new char[] {'R', 'R', 'R', 'G', 'G', 'B', 'B'};
+			char[] order = new char[] {'R', 'G', 'B'};
+			assertArrayEquals(output, sortArray(input, order));
+		}
+	}
+}
---
+++ src/p20to40/Problem37.java
@@ -0,0 +1,55 @@
+package p20to40;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+//Good morning! Here's your coding interview problem for today.
+//
+//This problem was asked by Google.
+//
+//The power set of a set is the set of all its subsets. Write a function that, given a set, 
+//generates its power set.
+//
+//For example, given the set {1, 2, 3}, it should return 
+//{{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+//
+//You may also use a list or array to represent a set.
+public class Problem37
+{
+	
+	public List<List<Integer>> getPowerSet(int[] array)
+	{
+		List<List<Integer>> powerSet = new ArrayList<List<Integer>>();
+		powerSet.add(new ArrayList<Integer>());
+		
+		for(int i : array)
+		{
+			int len = powerSet.size();
+			for(int j = 0; j < len; j++)
+			{
+				List<Integer> newList= new ArrayList<Integer>(powerSet.get(j));
+				newList.add(i);
+				powerSet.add(newList);
+			}
+		}
+		return powerSet;
+	}
+	
+	@Nested
+	public class TestProblem37
+	{
+		@Test
+		void test()
+		{
+			int[] input = new int[] {1,2,3};
+			String output = "[[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]";
+			// I am testing against string here since lists are "hard" to construct the list for testing.
+			assertEquals(output, getPowerSet(input).toString());
+		}
+	}
+}
