ec760da16f068bc3e0d72f290e00899db7f8289a
felix <felix@jamelin.de>
2018-11-05 11:58:56 +0000

    done 117, interesting question

---
+++ src/p90to120/Problem117.java
@@ -0,0 +1,54 @@
+package p90to120;
+
+import java.util.List;
+import java.util.Stack;
+
+//Good morning! Here's your coding interview problem for today.
+//
+//This problem was asked by Facebook.
+//
+//Given a binary tree, return the level of the tree with minimum sum.
+public class Problem117
+{
+	public int getLevelWithMinSum(Node root)
+	{
+		int min = Integer.MAX_VALUE;
+		int minLevel = 0;
+		int level = 0;
+		int sum = 0;
+		Stack<Node> thisLevel = new Stack<Node>();
+		thisLevel.push(root);
+		Stack<Node> nextLevel;
+		
+		do
+		{
+			nextLevel = new Stack<Node>();
+			while(!thisLevel.isEmpty())
+			{
+				Node node = thisLevel.pop();
+				sum =+ node.getValue();
+				nextLevel.addAll(node.getChildren());
+			}
+			if(min > sum)
+			{
+				min = sum;
+				minLevel = level;
+			}
+			
+			sum = 0;
+			level++;
+			thisLevel = nextLevel;
+		}while(!thisLevel.isEmpty());
+		
+		return minLevel;
+	}
+	
+	
+	
+	public interface Node
+	{
+		List<Node> getChildren();
+		int getValue();
+//		boolean hasChildren();
+	}
+}
