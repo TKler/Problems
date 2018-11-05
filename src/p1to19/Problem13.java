72674260029ad7d8c202e9b4103ad2cd5ca048ed
felix <felix@jamelin.de>
2018-11-05 14:09:52 +0000

    added 13

---
+++ src/p1to19/Problem13.java
@@ -0,0 +1,72 @@
+package p1to19;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+//Good morning! Here's your coding interview problem for today.
+//
+//This problem was asked by Amazon.
+//
+//Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+//
+//For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+public class Problem13
+{
+	public int getLengthofLongestSubstringWithDistinctChars(String word, int distinctChars)
+	{
+		int max = 0;
+		Map<Character, Integer> charCount = new HashMap<Character, Integer>(distinctChars);
+		int startIndex = 0;
+		int endIndex = 0;
+		
+		while(endIndex < word.length()-1)
+		{
+			while(endIndex < word.length()-1 && charCount.size() <= distinctChars)
+			{
+				charCount.merge(word.charAt(endIndex), 1, Integer::sum);
+				endIndex++;
+			}
+			
+			max = Math.max(max, endIndex - startIndex +1 -1); // both index are on point so +1, but -1 since we count one char too many
+			
+			while(charCount.size() > distinctChars)
+			{
+				Character charAtStart = word.charAt(startIndex);
+				if(charCount.get(charAtStart) > 1)
+					charCount.merge(charAtStart, -1, Integer::sum);
+				else
+					charCount.remove(charAtStart);
+				
+				startIndex++;
+			}
+		}
+		
+		return max;
+	}
+	
+	@Nested
+	public class TestProblem13
+	{
+		@Test
+		void test()
+		{
+			int inputInt = 2;
+			String inputWord = "abcba";
+			int output = 3;
+			assertEquals(output, getLengthofLongestSubstringWithDistinctChars(inputWord, inputInt));
+		}
+		@Test
+		void test2()
+		{
+			int inputInt = 2;
+			String inputWord = "abcb";
+			int output = 3;
+			assertEquals(output, getLengthofLongestSubstringWithDistinctChars(inputWord, inputInt));
+		}
+	}
+}
